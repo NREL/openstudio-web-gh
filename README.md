f0334a0bf3ab8a0ea4eb35b874156f308d3be02d
Alex Swindler <Alex.Swindler@nrel.gov>
2021-11-08 12:59:28 -0700

    Updated releases

--- README.md
+++ README.md
@@ -4,22 +4,6 @@
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/openstudio-web-gh`. The app will automatically reload if you change any of the source files.
 
-## Code scaffolding
+## OpenStudio Releases
 
-Run `ng generate component componentName` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build and Deploy
-
-Run `ng deploy` to build the project and push the build to the `gh-pages` branch on GitHub. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+Update `src/app/shared/classes/releases.ts` with the latest versions and urls
--- src/app/shared/classes/releases.ts
+++ src/app/shared/classes/releases.ts
@@ -3,38 +3,38 @@ import { PluginRelease, Release } from './constants';
 export const releases: (Release | PluginRelease)[] = [{
   title: 'OpenStudio Release',
   type: 'sdk',
-  version: '3.2.1',
-  sha: 'bdbdbc9da6',
-  winUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.2.1/OpenStudio-3.2.1+bdbdbc9da6-Windows.exe',
-  macUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.2.1/OpenStudio-3.2.1+bdbdbc9da6-Darwin.dmg',
-  linuxUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.2.1/OpenStudio-3.2.1+bdbdbc9da6-Ubuntu-20.04.deb'
+  version: '3.3.0',
+  sha: 'ad235ff36e',
+  winUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.3.0/OpenStudio-3.3.0+ad235ff36e-Windows.exe',
+  macUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.3.0/OpenStudio-3.3.0+ad235ff36e-Darwin.dmg',
+  linuxUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.3.0/OpenStudio-3.3.0+ad235ff36e-Ubuntu-20.04.deb'
 }, {
   title: 'OpenStudio Application Release',
   type: 'openstudio-application',
-  version: '1.2.0',
-  sha: '72d719fabe',
-  winUrl: 'https://github.com/openstudiocoalition/OpenStudioApplication/releases/download/v1.2.0/OpenStudioApplication-1.2.0+72d719fabe-Windows.exe',
-  macUrl: 'https://github.com/openstudiocoalition/OpenStudioApplication/releases/download/v1.2.0/OpenStudioApplication-1.2.0+72d719fabe-macOS10.14-x86_64.dmg',
-  linuxUrl: 'https://github.com/openstudiocoalition/OpenStudioApplication/releases/download/v1.2.0/OpenStudioApplication-1.2.0+72d719fabe-Ubuntu20.04.deb'
+  version: '1.2.1',
+  sha: '29888f9a87',
+  winUrl: 'https://github.com/openstudiocoalition/OpenStudioApplication/releases/download/v1.2.1/OpenStudioApplication-1.2.1+29888f9a87-Windows.exe',
+  macUrl: 'https://github.com/openstudiocoalition/OpenStudioApplication/releases/download/v1.2.1/OpenStudioApplication-1.2.1+29888f9a87-macOS10.14-x86_64.dmg',
+  linuxUrl: 'https://github.com/openstudiocoalition/OpenStudioApplication/releases/download/v1.2.1/OpenStudioApplication-1.2.1+29888f9a87-Ubuntu20.04.deb'
 }, {
   title: 'OpenStudio PAT Release',
   type: 'pat',
-  version: '3.2.1',
-  winUrl: 'https://github.com/NREL/OpenStudio-PAT/releases/download/v3.2.1/ParametricAnalysisTool-3.2.1-Windows.exe',
-  macUrl: 'https://github.com/NREL/OpenStudio-PAT/releases/download/v3.2.1/ParametricAnalysisTool-3.2.1-Darwin.dmg',
-  linuxUrl: 'https://github.com/NREL/OpenStudio-PAT/releases/download/v3.2.1/ParametricAnalysisTool-3.2.1-Linux.deb'
+  version: '3.3.0',
+  winUrl: 'https://github.com/NREL/OpenStudio-PAT/releases/download/v3.3.0/ParametricAnalysisTool-3.3.0-Windows.exe',
+  macUrl: 'https://github.com/NREL/OpenStudio-PAT/releases/download/v3.3.0/ParametricAnalysisTool-3.3.0-Darwin.dmg',
+  linuxUrl: 'https://github.com/NREL/OpenStudio-PAT/releases/download/v3.3.0/ParametricAnalysisTool-3.3.0-Linux.deb'
 }, {
   title: 'SketchUp Plugin Release',
   type: 'plugin',
-  version: '1.2.0',
-  url: 'https://github.com/openstudiocoalition/openstudio-sketchup-plugin/releases/download/v1.2.0/openstudio-sketchup-plugin.rbz'
+  version: '1.2.1',
+  url: 'https://github.com/openstudiocoalition/openstudio-sketchup-plugin/releases/download/v1.2.1/openstudio-sketchup-plugin.rbz'
 }];
 
 export const developerRelease: Release = {
   title: 'Latest Development Package',
   type: 'developer-sdk',
-  version: '3.2.1',
-  winUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.2.1/OpenStudio-3.2.1+bdbdbc9da6-Windows.exe',
-  macUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.2.1/OpenStudio-3.2.1+bdbdbc9da6-Darwin.dmg',
-  linuxUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.2.1/OpenStudio-3.2.1+bdbdbc9da6-Ubuntu-20.04.deb'
+  version: '3.3.0',
+  winUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.3.0/OpenStudio-3.3.0+ad235ff36e-Windows.exe',
+  macUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.3.0/OpenStudio-3.3.0+ad235ff36e-Darwin.dmg',
+  linuxUrl: 'https://github.com/NREL/OpenStudio/releases/download/v3.3.0/OpenStudio-3.3.0+ad235ff36e-Ubuntu-20.04.deb'
 };
