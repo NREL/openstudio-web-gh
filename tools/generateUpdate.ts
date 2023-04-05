3497ceb1b15158d14a0d9327bbe3056dfc8aada7
Alex Swindler <Alex.Swindler@nrel.gov>
2023-04-05 11:39:21 -0600

    Angular v15 upgrade

--- tools/generateUpdate.ts
+++ tools/generateUpdate.ts
@@ -1,16 +1,12 @@
 import * as fs from 'fs';
 import * as path from 'path';
-import { Release } from '../src/app/shared/classes/constants';
 import { releases } from '../src/app/shared/classes/releases';
 
 const release = releases.find(({type}) => type === 'sdk');
 if (!release) {
   throw new Error('SDK release not found');
 }
-const sdkRelease = release as Release;
-
-const version = sdkRelease.version;
-const sha = sdkRelease.sha;
+const {sha, version} = release;
 
 const output = `<html>
 <openstudio>
@@ -23,5 +19,7 @@ const output = `<html>
   </release>
 </openstudio>
 </html>`;
+
+fs.mkdirSync(path.resolve(__dirname, '../dist'), {recursive: true});
 const outputFile = path.resolve(__dirname, '../dist/update.html');
 fs.writeFileSync(outputFile, output);
