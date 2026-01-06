a4862b24aa3460a50963fd5c2cc34804727b3c92
kflemin <2205659+kflemin@users.noreply.github.com>
2026-01-06 10:55:37 -0700

    additional news fixes

--- src/app/shared/classes/developer-news.ts
+++ src/app/shared/classes/developer-news.ts
@@ -11,11 +11,11 @@ export const developerNews: NewsItem[] = [
   html: 'OpenStudio 3.9.0 SDK has been released. 3.9.0 is a major release, this update includes performance improvements and bug fixes. View the complete <a href="https://github.com/NREL/OpenStudio/releases/tag/v3.9.0">Release Notes</a> for more information.'
 }, {
   title: 'OpenStudio 3.8.0 SDK Release',
-  time: '2024-5-20 12:00:00 -0600',
+  time: '2024-05-20 12:00:00 -0600',
   html: 'OpenStudio 3.8.0 SDK has been released. 3.8.0 is a major release, this update includes performance improvements and bug fixes. View the complete <a href="https://github.com/NREL/OpenStudio/releases/tag/v3.8.0">Release Notes</a> for more information.'
 }, {
   title: 'OpenStudio 3.7.0 SDK Release',
-  time: '2023-11-20 12:00:00 -0600',
+  time: '2023-11-20 12:00:00 -0700',
   html: 'OpenStudio 3.7.0 SDK has been released. 3.7.0 is a major release, this update includes performance improvements and bug fixes. View the complete <a href="https://github.com/NREL/OpenStudio/releases/tag/v3.7.0">Release Notes</a> for more information.'
 }, {
   title: 'OpenStudio 3.6.1 SDK Release',
